d0c738222adf86a72e9d42f95392e12b962540cf
Jugal Shah <23151283+shahjugal@users.noreply.github.com>
2023-01-07 18:41:28 +0530

    Minor Change

--- formulae_app/lib/Pages/LoginUI.dart
+++ formulae_app/lib/Pages/LoginUI.dart
@@ -12,95 +12,97 @@ class LoginUI extends StatelessWidget {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              // ignore: prefer_const_constructors
-              SizedBox(
-                height: 50,
-              ),
-              // ignore: prefer_const_constructors
-              Text(
-                "Get Started",
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
                 // ignore: prefer_const_constructors
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              // ignore: prefer_const_constructors
-              SizedBox(
-                height: 20,
-              ),
-              // ignore: prefer_const_constructors
-              Text(
-                "Get your ass in!",
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w300,
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              fTextBox("University Email", false),
-              SizedBox(
-                height: 25,
-              ),
-              fTextBox("Password", true),
-              SizedBox(height: 30),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 42, 42, 42),
-                      borderRadius:
-                          BorderRadiusDirectional.all(Radius.circular(10)),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Sign in",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
+                // ignore: prefer_const_constructors
+                Text(
+                  "Get Started",
+                  // ignore: prefer_const_constructors
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                // ignore: prefer_const_constructors
+                SizedBox(
+                  height: 10,
+                ),
+                // ignore: prefer_const_constructors
+                Text(
+                  "Get your ass in!",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Icon(
+                  Icons.person_pin_circle_outlined,
+                  size: 175,
+                  color: Color.fromARGB(255, 42, 42, 42),
+                ),
+                const SizedBox(
+                  height: 17,
+                ),
+                fTextBox("University Email", false),
+                SizedBox(
+                  height: 10,
+                ),
+                fTextBox("Password", true),
+                SizedBox(height: 10),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 42, 42, 42),
+                        borderRadius:
+                            BorderRadiusDirectional.all(Radius.circular(10)),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sign in",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: Column(
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    Text(
-                      "Not yet registered?",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                SizedBox(height: 15),
+                Center(
+                  child: Column(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Text(
+                        "Not yet registered?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Forgotten Password?",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                      SizedBox(
+                        height: 10,
                       ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Other options.",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        "Other options.",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
