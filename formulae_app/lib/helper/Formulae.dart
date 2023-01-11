0e35a8858967ee30865732b05ef0db95a8752d35
Jugal Shah <23151283+shahjugal@users.noreply.github.com>
2023-01-11 07:43:54 +0530

    made a class which will store all attributes of formula

---
+++ formulae_app/lib/helper/Formulae.dart
@@ -0,0 +1,29 @@
+// Objects of this class holds the informations regarding particular formulae.
+
+class Formulae {
+  int? id;
+  String? name;
+  String? description;
+  String? latexString;
+  String? applications;
+
+  List<String>? imageLinks;
+  List<String>? ytLinks;
+  List<String>? driveLinks;
+  List<String>? researchLinks;
+  List<String>? relatedCourses;
+  List<String>? tags;
+
+  Formulae(
+      {this.id,
+      this.name,
+      this.description,
+      this.latexString,
+      this.applications,
+      this.imageLinks,
+      this.ytLinks,
+      this.driveLinks,
+      this.researchLinks,
+      this.relatedCourses,
+      this.tags});
+}
